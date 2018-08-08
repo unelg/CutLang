1e622f6b081b75973d8bbfc7e0542e33c2c11572
Anna-Monica <34393930+anna-monica@users.noreply.github.com>
2018-08-08 16:33:53 +0200

    .

--- parser_test/b.hpp
+++ parser_test/b.hpp
@@ -51,45 +51,62 @@
      QGJET = 267,
      NUMET = 268,
      METLV = 269,
-     PHI = 270,
-     ETA = 271,
-     ABSETA = 272,
-     PT = 273,
-     PZ = 274,
-     NBF = 275,
-     DR = 276,
-     DPHI = 277,
-     NELE = 278,
-     NMUO = 279,
-     NLEP = 280,
-     NPHO = 281,
-     NJET = 282,
-     NBJET = 283,
-     NQGJET = 284,
-     HT = 285,
-     METMWT = 286,
-     MWT = 287,
-     MET = 288,
-     ALL = 289,
-     LEPSF = 290,
-     FILLHISTOS = 291,
-     NB = 292,
-     ID = 293,
-     SIN = 294,
-     COS = 295,
-     TAN = 296,
-     INT = 297,
-     OR = 298,
-     AND = 299,
-     LT = 300,
-     GT = 301,
-     LE = 302,
-     GE = 303,
-     EQ = 304,
-     NE = 305,
-     IRG = 306,
-     ERG = 307,
-     Unary = 308
+     MINPTM = 270,
+     MINPTG = 271,
+     MINPTJ = 272,
+     MINPTE = 273,
+     MAXETAM = 274,
+     MAXETAE = 275,
+     MAXETAG = 276,
+     MAXETAJ = 277,
+     MAXMET = 278,
+     TRGE = 279,
+     TRGM = 280,
+     LVLO = 281,
+     ATLASOD = 282,
+     CMSOD = 283,
+     DELPHES = 284,
+     FCC = 285,
+     LHCO = 286,
+     PHI = 287,
+     ETA = 288,
+     ABSETA = 289,
+     PT = 290,
+     PZ = 291,
+     NBF = 292,
+     DR = 293,
+     DPHI = 294,
+     NELE = 295,
+     NMUO = 296,
+     NLEP = 297,
+     NPHO = 298,
+     NJET = 299,
+     NBJET = 300,
+     NQGJET = 301,
+     HT = 302,
+     METMWT = 303,
+     MWT = 304,
+     MET = 305,
+     ALL = 306,
+     LEPSF = 307,
+     FILLHISTOS = 308,
+     NB = 309,
+     ID = 310,
+     SIN = 311,
+     COS = 312,
+     TAN = 313,
+     INT = 314,
+     OR = 315,
+     AND = 316,
+     LT = 317,
+     GT = 318,
+     LE = 319,
+     GE = 320,
+     EQ = 321,
+     NE = 322,
+     IRG = 323,
+     ERG = 324,
+     Unary = 325
    };
 #endif
 /* Tokens.  */
@@ -105,59 +122,76 @@
 #define QGJET 267
 #define NUMET 268
 #define METLV 269
-#define PHI 270
-#define ETA 271
-#define ABSETA 272
-#define PT 273
-#define PZ 274
-#define NBF 275
-#define DR 276
-#define DPHI 277
-#define NELE 278
-#define NMUO 279
-#define NLEP 280
-#define NPHO 281
-#define NJET 282
-#define NBJET 283
-#define NQGJET 284
-#define HT 285
-#define METMWT 286
-#define MWT 287
-#define MET 288
-#define ALL 289
-#define LEPSF 290
-#define FILLHISTOS 291
-#define NB 292
-#define ID 293
-#define SIN 294
-#define COS 295
-#define TAN 296
-#define INT 297
-#define OR 298
-#define AND 299
-#define LT 300
-#define GT 301
-#define LE 302
-#define GE 303
-#define EQ 304
-#define NE 305
-#define IRG 306
-#define ERG 307
-#define Unary 308
+#define MINPTM 270
+#define MINPTG 271
+#define MINPTJ 272
+#define MINPTE 273
+#define MAXETAM 274
+#define MAXETAE 275
+#define MAXETAG 276
+#define MAXETAJ 277
+#define MAXMET 278
+#define TRGE 279
+#define TRGM 280
+#define LVLO 281
+#define ATLASOD 282
+#define CMSOD 283
+#define DELPHES 284
+#define FCC 285
+#define LHCO 286
+#define PHI 287
+#define ETA 288
+#define ABSETA 289
+#define PT 290
+#define PZ 291
+#define NBF 292
+#define DR 293
+#define DPHI 294
+#define NELE 295
+#define NMUO 296
+#define NLEP 297
+#define NPHO 298
+#define NJET 299
+#define NBJET 300
+#define NQGJET 301
+#define HT 302
+#define METMWT 303
+#define MWT 304
+#define MET 305
+#define ALL 306
+#define LEPSF 307
+#define FILLHISTOS 308
+#define NB 309
+#define ID 310
+#define SIN 311
+#define COS 312
+#define TAN 313
+#define INT 314
+#define OR 315
+#define AND 316
+#define LT 317
+#define GT 318
+#define LE 319
+#define GE 320
+#define EQ 321
+#define NE 322
+#define IRG 323
+#define ERG 324
+#define Unary 325
 
 
 
 
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
-#line 28 "parse.y"
+#line 31 "parse.y"
 {
         Node* node;
 	double real;
 	char* s;//ADD POINTER TO NODE unique_ptr?
 }
 /* Line 1529 of yacc.c.  */
-#line 161 "b.hpp"
+#line 195 "b.hpp"
 	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
