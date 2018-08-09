61d11e05dfff443272fe14d48edc18600d188a31
Anna-Monica <34393930+anna-monica@users.noreply.github.com>
2018-08-09 19:02:56 +0200

    .

---
+++ analysis_core/SearchNode.h
@@ -0,0 +1,37 @@
+#ifndef SearchNode_h
+#define SearchNode_h
+
+#include <stdio.h>
+#include <math.h>
+#include <list>
+#include "Node.h"
+//takes care of Minimizing/Maximising
+class SearchNode : public Node{
+private:
+    double (*f)(double, double);
+public:
+    SearchNode(double (*func)(double, double), Node* l, Node* r, std::string s){
+        f=func;
+        symbol=s;
+        left=l;
+        right=r;
+    }
+
+    virtual double evaluate(AnalysisObjects* ao){
+            std::vector<myParticle>* particles=left->getParticles();
+            list<int> indices;
+            for(int i=0;i<particles->size();i++){
+                if(particles->at(i).index<0) indices.push_back(i);
+            }
+            int level=indices.size();
+
+            
+            return (*f)(left->evaluate(ao),right->evaluate(ao));
+   
+    }
+    virtual ~SearchNode() {
+//        if (left!=NULL) delete left;
+//        if (right!=NULL) delete right;
+    }
+
+};
