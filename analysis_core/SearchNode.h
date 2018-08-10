a475eecc4f3bff9be3a3042710e984045c0abdd1
Anna-Monica <34393930+anna-monica@users.noreply.github.com>
2018-08-10 16:37:09 +0200

    nested loop

--- analysis_core/SearchNode.h
+++ analysis_core/SearchNode.h
@@ -9,6 +9,26 @@
 class SearchNode : public Node{
 private:
     double (*f)(double, double);
+
+    void performInnerOperation(vector<int> v){
+        for(auto i=v.begin();i!=v.end();i++){
+            cout<<*i<<" ";
+        }
+        cout<<"eND"<<endl;
+    ;
+    }
+
+    void runNestedLoop( int start, int N, int level, int maxDepth, vector<int> v) {
+    if(level==maxDepth) performInnerOperation (v);
+    else{
+        for (int x = start; x <= N; x++ ) {
+            //check if particle x is forbidden
+            v.push_back(x); //add the current value
+            runNestedLoop( x+1 , N, level + 1, maxDepth, v );
+            v.pop_back();//remove the value
+        }
+    }
+}
 public:
     SearchNode(double (*func)(double, double), Node* l, Node* r, std::string s){
         f=func;
@@ -25,12 +45,17 @@ public:
             for(int i=0;i<particles->size();i++){
                 if(particles->at(i).index<0) indices.push_back(i);
             }
-            int level=indices.size();
+            int MaxDepth=indices.size();//number of nested loops needed
+
+            
+
 
             
             return (*f)(left->evaluate(ao),right->evaluate(ao));
    
     }
+
+
     virtual ~SearchNode() {
 //        if (left!=NULL) delete left;
 //        if (right!=NULL) delete right;
