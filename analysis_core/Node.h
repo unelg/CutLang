8251cbc4d378cf024694d5fef3e119c6a5f79c13
Anna-Monica <34393930+anna-monica@users.noreply.github.com>
2018-08-15 20:11:05 +0200

    .

--- analysis_core/Node.h
+++ analysis_core/Node.h
@@ -17,6 +17,7 @@
 #include "dbx_muon.h"
 #include "dbx_jet.h"
 #include "analysis_core.h"
+#include "myParticle.h"
 
 struct AnalysisObjects {
     std::vector<dbxMuon>     muos;
@@ -33,32 +34,17 @@ protected:
     Node* left;
     Node* right;
     std::string symbol;
-    void getStr(TString *ss){
-        if(left!=NULL) left->getStr(ss); 
-               (*ss)+=" ";
-               (*ss)+=symbol;
-        if(right!=NULL) right->getStr(ss);
-    }
-    void display(std::string indent){
-        if(left!=NULL) left->display(indent+"     "); 
-        std::cout<<indent+symbol<<std::endl;
-        if(right!=NULL) right->display("\n"+indent+"     ");
-    }
+    void getStr(TString *ss);
+    void display(std::string indent);
 public:
 
     virtual void Reset()=0;
     virtual void getParticles(std::vector<myParticle *>* particles)=0;
-    void display(){ this->display(""); }
-    virtual TString getStr(){ TString ss; this->getStr(&ss); return ss; }
+    void display();
+    virtual TString getStr();
     virtual double evaluate(AnalysisObjects* ao)=0;
-    virtual ~ Node(){
-    }
-    std::vector<dbxJet> tagJets(AnalysisObjects *ao, int jtype) {                   
-                                std::vector<dbxJet>      rjets;
-                                for (size_t jj=0; jj<ao->jets.size(); jj++) 
-                                  if (ao->jets.at(jj).isbtagged_77() == jtype) {rjets.push_back(ao->jets.at(jj)); }
-                                return rjets;
-    }
+    virtual ~ Node();
+    std::vector<dbxJet> tagJets(AnalysisObjects *ao, int jtype) ;
 
 };
 
