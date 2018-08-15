99e593fe8c693ff293fd49b63704fb4d8c3bd7cb
Anna-Monica <34393930+anna-monica@users.noreply.github.com>
2018-08-15 20:11:05 +0200

    .

--- analysis_core/FuncNode.h
+++ analysis_core/FuncNode.h
@@ -11,8 +11,8 @@
 #include <vector>
 #include "myParticle.h"
 #include "Node.h"
-using namespace std;
 
+using namespace std;
 //#define _CLV_
 
 #ifdef _CLV_
@@ -20,7 +20,7 @@ using namespace std;
 #else
 #define DEBUG(a)
 #endif
-
+using namespace std;
 //takes care of functions with arguments
 class FuncNode : public Node{
 private:
@@ -34,181 +34,44 @@ protected:
     std::vector<myParticle> originalParticles;
     dbxParticle myPart;
 
-    virtual void ResetParticles(){
-            for(int i=0;i<originalParticles.size();i++){
-            DEBUG("Recall orig i:"<<originalParticles[i].index);
-                    *(inputParticles[i])=originalParticles[i];
-            }
-        }
+    virtual void ResetParticles();
     
-    void partConstruct(AnalysisObjects *ao, std::vector<myParticle*> *input, dbxParticle* inputPart){
-        inputPart->Reset();
-        
-        for(vector<myParticle*>::iterator i=input->begin();i!=input->end();i++){
-                int atype=(*i)->type;
-                switch (atype) { 
-                                                case 0: inputPart->setTlv(  inputPart->lv()+ao->muos[ (*i)->index ].lv() ); // 0 is muon
-                                                        inputPart->setCharge(inputPart->q()+ao->muos[ (*i)->index ].q()  );
-                                                        inputPart->setIsTight(ao->muos[ (*i)->index ].isZCand()); // i am overloading the isTight
-                                                        DEBUG("muon:"<<(*i)->index <<"  q:"<<ao->muos[ (*i)->index ].q()<<"  ");
-                                                        break;
-                                                case 1: inputPart->setTlv(  inputPart->lv()+ao->eles[ (*i)->index ].lv() ); // 1 is electron
-                                                        inputPart->setCharge(inputPart->q()+ao->eles[ (*i)->index ].q()  );
-                                                        inputPart->setIsTight(ao->eles[ (*i)->index ].isZCand()); // i am overloading the isTight
-                                                        DEBUG("electron:"<<(*i)->index<<"  ");
-                                                        break;
-                                            	case 2: DEBUG("jet:"<<(*i)->index<<" ");
-                                                        inputPart->setTlv(inputPart->lv()   +   ao->jets[ (*i)->index ].lv() ); // 2 is any jet
-                                                        //inputPart->setTlv( ao->jets[ (*i)->index ].lv() ); // 2 is any jet
-                                                        break;
-                                                case 3: inputPart->setTlv(inputPart->lv()+tagJets(ao, 1)[ (*i)->index ].lv() ); // 3 is a b jet
-                                                        DEBUG("b-jet:"<<(*i)->index<<"  ");
-                                                        break;
-                                                case 4: inputPart->setTlv(inputPart->lv()+tagJets(ao, 0)[ (*i)->index ].lv()); // 4 is light jet
-                                                        DEBUG("qgjet:"<<(*i)->index<<" ");
-                                                        break;
-                                                case 5: v_eta=ao->muos[(*i)->index].lv().Eta();
-                                                        ametlv.SetPtEtaPhiM(ao->met.Mod(), v_eta,ao->met.Phi(),0);
-                                                        inputPart->setTlv(inputPart->lv()+ametlv); // met4v is v from MET using same eta approx.
-                                                        DEBUG("muMET ");
-                                                        break;
-                                                case 6: v_eta=ao->eles[ (*i)->index ].lv().Eta();
-                                                        ametlv.SetPtEtaPhiM(ao->met.Mod(), v_eta,ao->met.Phi(),0);
-                                                        inputPart->setTlv(inputPart->lv()+ametlv); // v from MET using same eta approx.
-                                                        DEBUG("eleMET ");
-                                                        break;
-                                                case 7: DEBUG("MET LV\n ");
-                                                        ametlv.SetPxPyPzE(ao->met.Px(), ao->met.Py(), 0, ao->met.Mod());
-                                                        inputPart->setTlv(inputPart->lv()+ametlv); // v from MET using same eta approx.
-                                                        break;
-                                                case 8: DEBUG("gamma:"<< (*i)->index <<" ");
-                                                        inputPart->setTlv(inputPart->lv()+ao->gams[ (*i)->index ].lv()); // 8 is gammas
-                                                        break;
+    void partConstruct(AnalysisObjects *ao, std::vector<myParticle*> *input, dbxParticle* inputPart);
 
-                                            default: std::cout<<"No such object! ERROR\n";
-                                                        break;
-                                } // end of case
-        }// end of for
-  
-    }
+    virtual void setParticleIndex(int order, int newIndex);
 
-    virtual void setParticleIndex(int order, int newIndex){
-                inputParticles.at(order)->index=newIndex;
-        }
-
-    virtual int getParticleIndex(int order){
-            return inputParticles.at(order)->index;
-    }
+    virtual int getParticleIndex(int order);
 
 public:
-    FuncNode(double (*func)(dbxParticle* apart ),std::vector<myParticle*> input,  std::string s ){
-        f=func;
-        symbol=s;
-        inputParticles=input;
-        myParticle apart;
-      for (int i=0; i<input.size(); i++){
-       DEBUG("orig i:"<<input[i]->index);
-       apart.index=input[i]->index;
-       apart.type=input[i]->type;
-       originalParticles.push_back(apart);
-      }
-        left=NULL;
-        right=NULL;
-    }
+    FuncNode(double (*func)(dbxParticle* apart ),std::vector<myParticle*> input,  std::string s );
     
-    virtual void Reset() override{
-        this->ResetParticles();
-    }
+    virtual void Reset() override;
 
-    virtual void getParticles(std::vector<myParticle *>* particles) override{
-        int size=particles->size();
-        for (int i=0; i<inputParticles.size(); i++){
-            
-            bool found=false;
-            for(int j=0;j<size;j++){
-                if (inputParticles[i]->index==particles->at(j)->index)
-                {
-                    found=true;break;
-                }
-            }
+    virtual void getParticles(std::vector<myParticle *>* particles) override;
 
-            if(!found){
-                particles->push_back(inputParticles[i]);
-            }
+    virtual double evaluate(AnalysisObjects* ao) override;
 
-            }    
-    }
-
-    virtual double evaluate(AnalysisObjects* ao) override{
-    partConstruct(ao, &inputParticles,&myPart);
-    return (*f)(&myPart );
-    }
-
-    virtual ~FuncNode() {}
+    virtual ~FuncNode();
 };
 
 
-double Qof( dbxParticle* apart){
-    double charge=apart->q();
-    DEBUG(" q:"<<charge<<"\t");
-    return charge;
-}
+double Qof( dbxParticle* apart);
 
-double Mof( dbxParticle* apart){
-    double mass=(apart->lv()).M();
-    DEBUG(" m:"<<mass<<"\t");
-    return mass;
-}
+double Mof( dbxParticle* apart);
 
-double Eof( dbxParticle* apart){
-    double energy=(apart->lv()).E();
-    DEBUG(" E:"<<energy<<"\t");
-    return energy;
-}
+double Eof( dbxParticle* apart);
 
-double Pof( dbxParticle* apart){
-    double p=(apart->lv()).P();
-    DEBUG(" P:"<<p<<"\t");
-    return p;
-}
+double Pof( dbxParticle* apart);
 
-double Pzof( dbxParticle* apart){
-    double pz=(apart->lv()).Pz();
-    DEBUG(" Pz:"<<pz<<"\t");
-    return pz;
-}
-double Ptof( dbxParticle* apart){
-    double pt=(apart->lv()).Pt();
-    DEBUG(" Pt:"<<pt<<"\t");
-    return pt;
-}
+double Pzof( dbxParticle* apart);
+double Ptof( dbxParticle* apart);
 
-double AbsEtaof( dbxParticle* apart){
-    double aeta=fabs((apart->lv()).Eta() );
-    DEBUG(" AEta:"<<aeta<<"\t");
-    return aeta;
-}
+double AbsEtaof( dbxParticle* apart);
 
-double Etaof( dbxParticle* apart){
-    double eta=(apart->lv()).Eta();
-    DEBUG(" Eta:"<<eta<<"\t");
-    return eta;
-}
+double Etaof( dbxParticle* apart);
 
-double Phiof( dbxParticle* apart){
-    double phi=(apart->lv()).Phi();
-    DEBUG(" phi:"<<phi<<"\t");
-    return phi;
-}
+double Phiof( dbxParticle* apart);
 
-double nbfof( dbxParticle* apart){
-    double phi=(apart->lv()).Phi();
-    DEBUG(" CORRECT ME NBF:"<<phi<<"\t");
-    return phi;
-}
-
-
-
-
+double nbfof( dbxParticle* apart);
 //other functions to be added
 #endif /* FuncNode_h */
--- analysis_core/LFuncNode.h
+++ analysis_core/LFuncNode.h
@@ -10,7 +10,7 @@
 #define LFuncNode_h
 #include <vector>
 #include "myParticle.h"
-#include "Node.h"
+#include "FuncNode.h"
 using namespace std;
 //takes care of functions with two arguments
 class LFuncNode : public FuncNode{
@@ -21,74 +21,19 @@ private:
     std::vector<myParticle> originalParticles2;
     dbxParticle myPart2;
 protected:
-    virtual void ResetParticles() override{
-          for(int i=0;i<originalParticles.size();i++){
-                *(inputParticles[i])=originalParticles[i];
+    virtual void ResetParticles() override;
 
-          }
-          for(int i=0;i<originalParticles2.size();i++){
-              *(inputParticles2[i])=originalParticles2[i];
-
-          }
-    }
-
-    virtual void setParticleIndex(int order, int newIndex) override{
-        if(order<inputParticles.size()){
-            inputParticles.at(order)->index=newIndex;
-        }
-        else{
-            inputParticles2.at(order-inputParticles.size())->index=newIndex;
-        }
-    }
-    
+    virtual void setParticleIndex(int order, int newIndex) override;
 public:
-    LFuncNode(double (*func)(dbxParticle* part1,dbxParticle* part2),std::vector<myParticle*> input1,std::vector<myParticle*> input2,std::string s )
-    : FuncNode(NULL,input1,s) {
-        f2=func;
-        inputParticles2=input2;
-        myParticle apart2;
-        for (int i=0; i<input2.size(); i++){
-           apart2.index=input2[i]->index;
-           apart2.type=input2[i]->type;
-         originalParticles2.push_back(apart2);
-        }
-    }
-    
-    
-
-    virtual void getParticles(std::vector<myParticle *>* particles) override{
-        cout<<"MODIFY TO CHECK FOR EXISTING INDICES in ListFuncNode------\n";
-        for (int i=0; i<inputParticles.size(); i++){
-            particles->push_back(inputParticles[i]);
-            }   
-        for (int i=0; i<inputParticles2.size(); i++){
-            particles->push_back(inputParticles2[i]);
-            } 
-    }
-    
-    virtual double evaluate(AnalysisObjects* ao)  override{
-        partConstruct(ao, &inputParticles,&myPart);
-        partConstruct(ao, &inputParticles2,&myPart2);
-        return (*f2)(&myPart,&myPart2);
-    }
-    virtual ~LFuncNode() {}
+    LFuncNode(double (*func)(dbxParticle* part1,dbxParticle* part2),std::vector<myParticle*> input1,std::vector<myParticle*> input2,std::string s );
+    virtual void getParticles(std::vector<myParticle *>* particles) override;
+    virtual double evaluate(AnalysisObjects* ao)  override;
+    virtual ~LFuncNode() ;
 };
 
-double dR(dbxParticle* apart,dbxParticle* apart2){
-    double deltaR=apart->lv().DeltaR(apart2->lv() );
-    DEBUG(" dR:"<<deltaR<<"\t");
-    return deltaR;
-}
+double dR(dbxParticle* apart,dbxParticle* apart2);
 
-double dPhi(dbxParticle* apart,dbxParticle* apart2){
-    double deltaPhi=fabs(apart->lv().DeltaPhi(apart2->lv() ) );
-    DEBUG(" dPhi:"<<deltaPhi<<"\t");
-    return deltaPhi;
-}
+double dPhi(dbxParticle* apart,dbxParticle* apart2);
 
-double dEta(dbxParticle* apart,dbxParticle* apart2){
-    double deltaEta=fabs(apart->lv().Eta() - apart2->lv().Eta() );
-    DEBUG(" dEta:"<<deltaEta<<"\t");
-    return deltaEta;
-}
+double dEta(dbxParticle* apart,dbxParticle* apart2);
 #endif /* LFuncNode_h */
