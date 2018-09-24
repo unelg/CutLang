e8ed34bd4f8e66ed26b50b35353b2af8539182a5
Gokhan Unel <gokhan.unel@cern.ch>
2018-09-24 17:30:01 +0200

    if(particles->size()==2) added for UserObjCuts

--- analysis_core/ObjectNode.cpp
+++ analysis_core/ObjectNode.cpp
@@ -51,10 +51,9 @@ void createNewJet(AnalysisObjects* ao,vector<Node*> *criteria,std::vector<myPart
         particles->clear();
         (*cutIterator)->getParticlesAt(particles,0);
         
-        if(particles->size()==1){
-            for (int ipart=ipart_max-1; ipart>=0; ipart--){
-                //setParticleIndex(0, ipart);----must be same as instrcution below
-                particles->at(0)->index=ipart;
+        if(particles->size()==1){ //-----------I have 1 particle set such as ELEs, JETs...
+            for (int ipart=ipart_max-1; ipart>=0; ipart--){ // I have all particles, jets, in an event.
+                particles->at(0)->index=ipart;             //----the index was originally 6213
                 bool ppassed=(*cutIterator)->evaluate(ao);
                 //DEBUG("\n tested:"<<ipart<< " passed =>"<< ppassed <<"\n");
                 if (!ppassed) ao->jets.erase(ao->jets.begin()+ipart);
@@ -62,10 +61,28 @@ void createNewJet(AnalysisObjects* ao,vector<Node*> *criteria,std::vector<myPart
         }
         else if(particles->size()==2){
             for (int ipart=ipart_max-1; ipart>=0; ipart--){
-               cout<<"!!!Case of two parameter function to be implemented!!!\n";
+                particles->at(0)->index=ipart;             //----the index was originally 6213
+
+           //if (sicarim_hiza_if_ulan_if==2) // do this intelligently
+            int ipart2_max;
+            cout << "P0t:"<<particles->at(0)->type<<"  P1t:"<<particles->at(1)->type<<"\n";
+            switch(){
+              case 0: ipart2_max=ao->muos.size(); // sometng like this...
+                     break;
+              case 1: ipart2_max=ao->eles.size();
+                     break;
             }
+            for (int kpart=ipart2_max-1; kpart>=0; kpart--){ 
+                particles->at(1)->index=kpart;             //----the index was originally 6213
+                bool ppassed=(*cutIterator)->evaluate(ao);
+                //DEBUG("\n tested:"<<ipart<< " passed =>"<< ppassed <<"\n");
+                if (!ppassed) { ao->jets.erase(ao->jets.begin()+ipart); break; }
+            } // end of loop over 2nd particle type
+
+            }
+        //cout<<"!!!Case of two parameter function to be implemented!!!\n";
         }
-    }
+    }// end of cutIterator loop
 }
 
 void createNewEle(AnalysisObjects* ao, vector<Node*> *criteria, std::vector<myParticle *> * particles) {
@@ -76,7 +93,6 @@ void createNewEle(AnalysisObjects* ao, vector<Node*> *criteria, std::vector<myPa
         
         if(particles->size()==1){
             for (int ipart=ipart_max-1; ipart>=0; ipart--){
-                //setParticleIndex(0, ipart);----must be same as instrcution below
                 particles->at(0)->index=ipart;
                 bool ppassed=(*cutIterator)->evaluate(ao);
                 //DEBUG("\n tested:"<<ipart<< " passed =>"<< ppassed <<"\n");
@@ -97,7 +113,6 @@ void createNewMuo(AnalysisObjects* ao, vector<Node*> *criteria, std::vector<myPa
         
         if(particles->size()==1){
             for (int ipart=ipart_max-1; ipart>=0; ipart--){
-                //setParticleIndex(0, ipart);----must be same as instrcution below
                 particles->at(0)->index=ipart;
                 bool ppassed=(*cutIterator)->evaluate(ao);
                 //DEBUG("\n tested:"<<ipart<< " passed =>"<< ppassed <<"\n");
