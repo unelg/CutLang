10fa974ae14c9c5af9f392543e8cd57942c6017f
arponpaul <46896025+arponpaul@users.noreply.github.com>
2019-07-11 12:27:56 +0200

    sort command

---
+++ analysis_core/SortNode.h
@@ -0,0 +1,31 @@
+#ifndef SortNode_h
+#define SortNode_h
+
+#include <stdio.h>
+#include <math.h>
+#include <list>
+#include <unordered_set>
+#include "myParticle.h"
+#include "Node.h"
+
+
+using namespace std;
+
+//takes care of Minimizing/Maximizing
+class SortNode : public Node{
+private:
+    double (*f)();
+
+    std::vector<myParticle *> particles;//pointers to particles in all nodes that have to be changed
+public:
+    SortNode(Node* funcVal, std::string s);
+    virtual double evaluate(AnalysisObjects* ao) override;
+    virtual void getParticles(std::vector<myParticle *>* particles) override;
+    virtual void getParticlesAt(std::vector<myParticle *>* particles, int index) override;
+    virtual void Reset() override;     
+    virtual ~SortNode() ;
+
+};
+
+
+#endif
