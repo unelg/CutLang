bdd75841bd82ce8989e8c924777c012b2ac21fcc
paradoxnant <ravelnant@gmail.com>
2019-08-20 15:14:25 +0200

    selection des bad combinations dans Denombrement

---
+++ analysis_core/Denombrement.h
@@ -0,0 +1,35 @@
+#ifndef DENOMBREMENT_H
+#define DENOMBREMENT_H
+#include <vector>
+
+using namespace std;
+
+
+class Denombrement
+{
+    public:
+        Denombrement(int, int);
+        Denombrement(int, int, vector<vector<int>>);
+        virtual ~Denombrement();
+
+        vector<vector<int>> output();
+        void affiche();
+        void affiche(int);
+        void affiche(int, int);
+        void itt();
+
+        void output(vector<vector<int>>&);
+
+    private:
+
+        int nJetRecontr;
+        int nJetTotal;
+
+        int nZ = (nJetTotal + 1)/nJetRecontr;
+
+        vector<vector<int>> out;
+        vector<vector<int>> tab_selection;
+        int itteration_ = 1;
+};
+
+#endif // DENOMBREMENT_H
