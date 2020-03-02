41f882a6c37dba362cada2f115129e96b21c7764
Jinen Setpal <52078103+JinenSetpal@users.noreply.github.com>
2020-03-02 20:10:17 +0530

    Script to run test cases

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,9 @@
+cd ; cd ./runs
+
+for i in $(ls |grep .adl); do
+        echo "Testing $i:"
+        echo "With CMSOD"
+        ./CLA.sh ../../cms_opendata_ttbar.root CMSOD -i $i -e 10000
+        echo "With ATLASOD"
+        ./CLA.sh ../../atla_opendata_had_ttbar.root -i $i -e 10000
+done
