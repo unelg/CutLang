c4af9629a88b16a9a240aedf070779368b24f8c1
Jinen Setpal <jinens8@gmail.com>
2020-03-03 15:44:42 +0530

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,9 +1,11 @@
 cd $HOME/work/CutLang/CutLang/runs
+wget http://www.hepforge.org/archive/cutlang/cms_opendata_ttbar.root
+wget http://www.hepforge.org/archive/cutlang/atla_opendata_had_ttbar.root
 
 for i in $(ls |grep .adl); do
         echo "Testing $i:"
         echo "With CMSOD"
-        ./CLA.sh ../../cms_opendata_ttbar.root CMSOD -i $i -e 10000
+        ./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000
         echo "With ATLASOD"
-        ./CLA.sh ../../atla_opendata_had_ttbar.root -i $i -e 10000
+        ./CLA.sh ./atla_opendata_had_ttbar.root -i $i -e 10000
 done
