773e6887dcb9e0bf0c231f247c64f2895929fbf8
Jinen Setpal <jinens8@gmail.com>
2020-03-08 18:03:10 +0530

    Update filter.sh

--- .github/workflows/filter.sh
+++ .github/workflows/filter.sh
@@ -4,4 +4,3 @@ echo $'Efficiency Charts:\n\n' | tee ./final_res.txt
 cat ./output.txt | grep -e Overall -e .adl -e SOD -e DELPHES | tee -a ./final_res.txt
 echo $'\n\nErrors:\n\n' | tee -a ./final_res.txt
 cat ./output.txt | grep -e 0x000 -e error -e segmentation -e WARNING -e ROOT -e aborting -e Fatal -e stack | tee -a ./final_res.txt
-sed -i 's/nan/[undefined]/g' final_res.txt
