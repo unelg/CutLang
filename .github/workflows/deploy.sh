26c5fb947f53b9f5e3af41a4e823170ca151a7da
Jinen Setpal <jinens8@gmail.com>
2020-03-09 08:46:17 +0530

    fixed incorrect directory

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $HOME/work/test/test/runs
+cd $HOME/work/CutLang/CutLang/runs
 wget http://www.hepforge.org/archive/cutlang/cms_opendata_ttbar.root
 wget http://www.hepforge.org/archive/cutlang/atla_opendata_had_ttbar.root
 wget -O delphes_events_ttbar.root https://docs.google.com/uc?export=download\&id=1P8Pv2hmV4QcMfNWmQTsuAkqIYcEzsuxt
