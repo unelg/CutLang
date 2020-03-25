689c86788bcff4a89fc6cd1deb3fcec7a4dbcf2a
Jinen Setpal <jinens8@gmail.com>
2020-03-25 23:52:14 +0530

    Update filter.sh

--- .github/workflows/filter.sh
+++ .github/workflows/filter.sh
@@ -3,4 +3,4 @@
 echo $'Efficacy Charts:\n' | tee ./efficacy_charts.txt 
 cat ./raw_output.txt | grep -e Overall -e .adl -e SOD -e DELPHES | tee -a ./efficacy_charts.txt
 echo $'Errors: \n' | tee ./errors.txt
-cat ./raw_output.txt | grep -e error -e WARNING  -e check -e syntax | tee -a ./errors.txt
+cat ./raw_output.txt | grep -A5 -B5 --colour -i error -i warning -i check -i syntax | tee -a ./errors.txt
