b28c971372815d2db47a59813606da861702fdb1
Jinen Setpal <jinens8@gmail.com>
2020-03-30 00:49:40 +0530

    Added .sh file for command input filtering

---
+++ .github/workflows/mail_body.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $HOME/work/CutLang/CutLang/.github/workflows/artifacts
+
+cat raw_output.txt | grep -e error -e WARNING -e check -e syntax -e 0x00 -e CLA.sh | tee ./temp.txt
+a=''
+for i in $(cat temp.txt | grep -A1 -e CLA.sh); do
+  if [ $`echo $a | grep -q CLA.sh` == $`echo $i | grep -q CLA.sh` ]; then
+    sed -i -e 's/$a//g' ./temp.txt
+  fi
+  a='$i'
+done
