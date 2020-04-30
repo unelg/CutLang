e3b4c6f73a5701f3d4abec59dd3860908f2443dd
JinenSetpal <jinens8@gmail.com>
2020-05-01 03:19:01 +0530

    unnecessary command inputs removed

--- .github/workflows/mail_body.sh
+++ .github/workflows/mail_body.sh
@@ -2,11 +2,11 @@
 
 cd $HOME/work/CutLang/CutLang/.github/workflows/artifacts
 
-root --version | tee ./temp.txt
+echo $`root --version` | tee ./temp.txt
 cat raw_output.txt | grep -e error -e WARNING -e syntax -e CLA.sh | tee -a ./temp.txt
 a=''
 for i in $(cat temp.txt | grep -A1 -e CLA.sh); do
-  if [ $`echo $a | grep -q CLA.sh` == $`echo $i | grep -q CLA.sh` ]; then
+  if [ $`echo $a | grep -q CLA.sh` == $`echo $i | grep -q CLA.sh` && $`cat $i | grep Aborted` != ""]; then
     sed -i -e 's/$a//g' ./temp.txt
   fi
   a='$i'
