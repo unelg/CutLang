43fd9221483cee19f48351e3a61e7d628f2fa8e6
JinenSetpal <jinens8@gmail.com>
2020-05-01 02:50:14 +0530

    updated mail body

--- .github/workflows/mail_body.sh
+++ .github/workflows/mail_body.sh
@@ -2,7 +2,8 @@
 
 cd $HOME/work/CutLang/CutLang/.github/workflows/artifacts
 
-cat raw_output.txt | grep -e error -e WARNING -e syntax -e CLA.sh | tee ./temp.txt
+root --version | tee ./temp.txt
+cat raw_output.txt | grep -e error -e WARNING -e syntax -e CLA.sh | tee -a ./temp.txt
 a=''
 for i in $(cat temp.txt | grep -A1 -e CLA.sh); do
   if [ $`echo $a | grep -q CLA.sh` == $`echo $i | grep -q CLA.sh` ]; then
