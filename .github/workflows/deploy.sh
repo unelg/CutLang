61be040a5eb13c43db56175ce6d0325e17fd1585
JinenSetpal <jinens8@gmail.com>
2020-05-01 02:50:14 +0530

    updated mail body

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -9,8 +9,11 @@ for i in $(ls |grep .adl); do
         echo "Testing $i:"
         echo "With CMSOD"
         ./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000
+        echo "./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000"
         echo "With ATLASOD"
         ./CLA.sh ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000
+        echo "./CLA.sh ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000"
         echo "With DELPHES"
         ./CLA.sh ./delphes_events_ttbar.root DELPHES -i $i -e 10000
+        echo "./CLA.sh ./delphes_events_ttbar.root DELPHES -i $i -e 10000"
 done
