dd6fecaa8a4702b4247fb05d9fd0d7b544a54c06
JinenSetpal <jinens8@gmail.com>
2020-05-03 19:06:12 +0530

    mail body finalised

--- .github/workflows/mail_body.sh
+++ .github/workflows/mail_body.sh
@@ -4,17 +4,20 @@ cd $HOME/work/CutLang/CutLang/.github/workflows/artifacts
 
 . $HOME/work/CutLang/CutLang/build/bin/thisroot.sh
 root --version 2>&1 | tee ./temp2.txt
-echo "" | tee -a temp2.txt
+echo ""
+cat raw_output.txt | grep -e error -e WARNING -e syntax -e CLA.sh > ./temp.txt
 
-cat raw_output.txt | grep -e error -e WARNING -e syntax -e CLA.sh | tee ./temp.txt
 a=''
 while IFS='' read -r i || [ -n "$i" ]; do
-        if echo $i | grep -q -e Aborted -e error; then
-                echo $a | tee -a temp2.txt
-                echo $i | tee -a temp2.txt
-                echo "" | tee -a temp2.txt
+        if echo $i | grep -q -e "Check the input file" -e Aborted; then
+                echo $a
+                echo $i
+                echo ""
+        elif echo $i | grep -q error; then
+                echo $a
         fi
         a=$i
 done < ./temp.txt
 rm temp.txt
 mv temp2.txt temp.txt
+
