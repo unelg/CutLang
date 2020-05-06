734b97b72424dd3b98b8b4c1361f624731ed26b0
JinenSetpal <jinens8@gmail.com>
2020-05-06 21:56:05 +0530

    ROOT 6.18/04; mail format changes

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,12 +8,12 @@ wget -O delphes_events_ttbar.root https://docs.google.com/uc?export=download\&id
 for i in $(ls |grep .adl); do
         echo "Testing $i:"
         echo "With CMSOD"
-        ./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000
         echo "./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000"
+        ./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000
         echo "With ATLASOD"
-        ./CLA.sh ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000
         echo "./CLA.sh ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000"
+        ./CLA.sh ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000
         echo "With DELPHES"
-        ./CLA.sh ./delphes_events_ttbar.root DELPHES -i $i -e 10000
         echo "./CLA.sh ./delphes_events_ttbar.root DELPHES -i $i -e 10000"
+        ./CLA.sh ./delphes_events_ttbar.root DELPHES -i $i -e 10000
 done
