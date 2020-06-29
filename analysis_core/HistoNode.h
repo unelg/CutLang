50f58b951f207276bc40a70d76db275e4f818ad5
alavee-aziz <66626190+alavee-aziz@users.noreply.github.com>
2020-06-30 01:24:40 +0600

    Add files via upload

--- analysis_core/HistoNode.h
+++ analysis_core/HistoNode.h
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class HistoNode : public Node{
+class Histo1DNode : public Node{
 private:
     std::string id;
     std::string Desciption;
@@ -26,23 +26,7 @@ private:
 
 public:
 
-    HistoNode( std::string id,std::string desc,int nx, float xmin, float xmax, int ny, float ymin, float ymax, Node* l, Node* r){
-        this->id=id;
-        Desciption=desc;
-        lowerLimitx=xmin;
-        upperLimitx=xmax;
-	lowerLimity=ymin;
-	upperLimity=ymax;
-        binsx=nx;
-	binsy=ny;
-        symbol="histo "+id+","+Desciption+","+std::to_string(xmin)+","+std::to_string(xmax)+","+std::to_string(nx)+","+std::to_string(ymin)+","+std::to_string(ymax)+","+std::to_string(ny);
-        ahisto2 = new TH2D(id.data(), Desciption.data(), binsx, lowerLimitx, upperLimitx, binsy, lowerLimity, upperLimity);
-	ahisto1 = NULL;
-        left=l;
-        right=r;
-	dim = 2;
-    }
-    HistoNode( std::string id,std::string desc,int n, float l1, float l2,Node* l){
+    Histo1DNode( std::string id,std::string desc,int n, float l1, float l2,Node* l){
         this->id=id;
         Desciption=desc;
         lowerLimitx=l1;
@@ -56,15 +40,63 @@ public:
     }
 virtual void Reset() override{ left->Reset(); } 
 
-virtual void getParticles(std::vector<myParticle *>* particles) override{
+	 virtual void getParticles(std::vector<myParticle *>* particles) override{
          left->getParticles(particles);
     }
-virtual void getParticlesAt(std::vector<myParticle *>* particles, int index) override{}
-double evaluate(AnalysisObjects* ao) override;
-virtual ~HistoNode(){
-  if (left!=NULL) delete left;
+ 	virtual void getParticlesAt(std::vector<myParticle *>* particles, int index) override{}
+     double evaluate(AnalysisObjects* ao) override;
+virtual ~Histo1DNode(){
+        if (left!=NULL) delete left;
 	if (right!=NULL) delete right;
-}
+    }
+    
+
+};
+
+class Histo2DNode : public Node{
+private:
+    std::string id;
+    std::string Desciption;
+    float lowerLimitx;
+    float upperLimitx;
+    float lowerLimity;
+    float upperLimity;
+    int binsx;
+    int binsy;
+    int dim = 1;
+    TH1D *ahisto1;
+    TH2D *ahisto2;
+    std::vector<myParticle*> inputParticles;
+
+
+public:
+
+    Histo2DNode( std::string id,std::string desc,int nx, float xmin, float xmax, int ny, float ymin, float ymax, Node* l, Node* r){
+        this->id=id;
+        Desciption=desc;
+        lowerLimitx=xmin;
+        upperLimitx=xmax;
+	lowerLimity=ymin;
+	upperLimity=ymax;
+        binsx=nx;
+	binsy=ny;
+        symbol="histo "+id+","+Desciption+","+std::to_string(xmin)+","+std::to_string(xmax)+","+std::to_string(nx)+","+std::to_string(ymin)+","+std::to_string(ymax)+","+std::to_string(ny);
+        ahisto2 = new TH2D(id.data(), Desciption.data(), binsx, lowerLimitx, upperLimitx, binsy, lowerLimity, upperLimity);
+	ahisto1 = NULL;
+        left=l;
+        right=r;
+    }
+virtual void Reset() override{ left->Reset(); } 
+
+	 virtual void getParticles(std::vector<myParticle *>* particles) override{
+         left->getParticles(particles);
+    }
+ 	virtual void getParticlesAt(std::vector<myParticle *>* particles, int index) override{}
+     double evaluate(AnalysisObjects* ao) override;
+virtual ~Histo2DNode(){
+        if (left!=NULL) delete left;
+	if (right!=NULL) delete right;
+    }
     
 
 };
