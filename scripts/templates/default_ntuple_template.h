116d555cf934f450d48f0357e3e510b4a6f31651
Burak Shen <tobuba2@gmail.com>
2021-06-08 14:06:04 +0300

    added default h files to addntuple

--- scripts/templates/default_ntuple_template.h
+++ scripts/templates/default_ntuple_template.h
@@ -1,10 +1,10 @@
 //////////////////////////////////////////////////////////
 // This class has been automatically generated on
-// ${date}
+// {{date}}
 //////////////////////////////////////////////////////////
 
-#ifndef ${name}_h
-#define ${name}_h
+#ifndef {{name}}_h
+#define {{name}}_h
 
 #include <TROOT.h>
 #include <TChain.h>
@@ -12,7 +12,7 @@
 
 // Header file for the classes stored in the TTree if any.
 
-class ${name} {
+class {{name}} {
 public :
    TTree          *fChain;   //!pointer to the analyzed TTree or TChain
    Int_t           fCurrent; //!current Tree number in a TChain
@@ -23,8 +23,8 @@ public :
 
    // List of branches
 
-   ${name}(TTree *tree=0);
-   virtual ~${name}();
+   {{name}}(TTree *tree=0);
+   virtual ~{{name}}();
    virtual Int_t    Cut(Long64_t entry);
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
@@ -36,8 +36,8 @@ public :
 
 #endif
 
-#ifdef ${name}_cxx
-${name}::${name}(TTree *tree) : fChain(0) 
+#ifdef {{name}}_cxx
+{{name}}::{{name}}(TTree *tree) : fChain(0) 
 {
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
@@ -53,19 +53,19 @@ ${name}::${name}(TTree *tree) : fChain(0)
    Init(tree);
 }
 
-${name}::~${name}()
+{{name}}::~{{name}}()
 {
    if (!fChain) return;
    delete fChain->GetCurrentFile();
 }
 
-Int_t ${name}::GetEntry(Long64_t entry)
+Int_t {{name}}::GetEntry(Long64_t entry)
 {
 // Read contents of entry.
    if (!fChain) return 0;
    return fChain->GetEntry(entry);
 }
-Long64_t ${name}::LoadTree(Long64_t entry)
+Long64_t {{name}}::LoadTree(Long64_t entry)
 {
 // Set the environment to read one entry
    if (!fChain) return -5;
@@ -78,7 +78,7 @@ Long64_t ${name}::LoadTree(Long64_t entry)
    return centry;
 }
 
-void ${name}::Init(TTree *tree)
+void {{name}}::Init(TTree *tree)
 {
    // The Init() function is called when the selector needs to initialize
    // a new tree or chain. Typically here the branch addresses and branch
@@ -97,7 +97,7 @@ void ${name}::Init(TTree *tree)
    Notify();
 }
 
-Bool_t ${name}::Notify()
+Bool_t {{name}}::Notify()
 {
    // The Notify() function is called when a new file is opened. This
    // can be either for a new TTree in a TChain or when when a new TTree
@@ -108,18 +108,18 @@ Bool_t ${name}::Notify()
    return kTRUE;
 }
 
-void ${name}::Show(Long64_t entry)
+void {{name}}::Show(Long64_t entry)
 {
 // Print contents of entry.
 // If entry is not specified, print current entry
    if (!fChain) return;
    fChain->Show(entry);
 }
-Int_t ${name}::Cut(Long64_t entry)
+Int_t {{name}}::Cut(Long64_t entry)
 {
 // This function may be called from Loop.
 // returns  1 if entry is accepted.
 // returns -1 otherwise.
    return 1;
 }
-#endif // #ifdef ${name}_cxx
+#endif // #ifdef {{name}}_cxx
