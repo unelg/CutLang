7ab7ff9baed93f0a3380c1e5dafacb9f0ccee161
Burak Shen <tobuba2@gmail.com>
2021-06-20 04:36:16 +0300

    addntuple template's content changed

--- scripts/templates/default_ntuple_content_template.C
+++ scripts/templates/default_ntuple_content_template.C
@@ -1,41 +1,41 @@
 {% extends 'default_ntuple_template.C' %}
 
 {% block GET_PHYS_TEMP_VAR %}
-{{CONTENT_GET_PHYS_TEMP_VAR}}
+${CONTENT_GET_PHYS_TEMP_VAR}
 {% endblock %}
 
 
 
 {% block PHOTONS %}
-{{CONTENT_PHOTONS}}
+${CONTENT_PHOTONS}
 {% endblock %}
 
 
 
 {% block MUONS %}
-{{CONTENT_MUONS}}
+${CONTENT_MUONS}
 {% endblock %}
 
 
 
 {% block ELECTRONS %}
-{{CONTENT_ELECTRONS}}
+${CONTENT_ELECTRONS}
 {% endblock %}
 
 
 
 {% block JETS %}
-{{CONTENT_JETS}}
+${CONTENT_JETS}
 {% endblock %}
 
 
 
 {% block MET %}
-{{CONTENT_MET}}
+${CONTENT_MET}
 {% endblock %}
 
 
 
 {% block POST_FINISH %}
-{{CONTENT_POST_FINISH}}
+${CONTENT_POST_FINISH}
 {% endblock %}
