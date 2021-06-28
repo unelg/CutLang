b92b3ad520d1768371fa3258cba49dd3ac6bcc20
Burak Shen <tobuba2@gmail.com>
2021-06-28 04:11:10 +0300

    updates for the package

--- runs/CLA.sh
+++ runs/CLA.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 export PATH=$PATH:$ROOTSYS/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib:.:/usr/lib:/usr/lib/system
+
+_RUNS_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+_SCRIPT=`realpath -s $0`
+SCRIPT=`realpath $0`            # /home/.../CutLang/runs/CLA.sh
+RUNS_PATH=`dirname $SCRIPT`    # /home/.../CutLang/runs
+WORK_PATH=`dirname $RUNS_PATH`  # /home/.../CutLang
+
+echo $SCRIPT
+echo $RUNS_PATH
+echo $WORK_PATH
+
 EVENTS=0
-INIFILE=CLA.ini
+INIFILE=$RUNS_PATH/CLA.ini
 VERBOSE=5000
 STRT=0
 DEPS=" "
@@ -13,7 +24,7 @@ if [ $# -lt 2 ]; then
  echo ERROR: not enough arguments
  echo $0  ROOTfile_name  ROOTfile_type [-h]
  cat $0 | grep '|-'|grep -v grep| cut -f1 -d')'
- echo "ROOT file type can be:"; grep "inptype ==" ../CLA/CLA.C | cut -f3 -d'=' | cut -f1 -d')'
+ echo "ROOT file type can be:"; grep "inptype ==" $WORK_PATH/CLA/CLA.C | cut -f3 -d'=' | cut -f1 -d')'
  exit 1
 fi
 
@@ -41,10 +52,10 @@ case $key in
 
     if [ $Nalgo -gt 1 ]; then
      echo Analysis with Multiple Regions
-     ../scripts/separate_algos.sh ${INIFILE}
+     $WORK_PATH/scripts/separate_algos.sh ${INIFILE}
     else
      echo Single Analysis
-     cp ${INIFILE} BP_1-card.ini
+     cp ${INIFILE} $PWD/BP_1-card.ini
      Nalgo=1
     fi
     shift # past argument
@@ -63,7 +74,7 @@ case $key in
     -h|--help)
     echo command line arguments:
     cat $0 | grep '|-'|grep -v grep| cut -f1 -d')'
-    echo "ROOT file type can be:"; grep "inptype ==" ../CLA/CLA.C | cut -f3 -d'=' | cut -f1 -d')'
+    echo "ROOT file type can be:"; grep "inptype ==" $WORK_PATH/CLA/CLA.C | cut -f3 -d'=' | cut -f1 -d')'
     exit 1
     ;;
     -d|--deps)
@@ -100,25 +111,27 @@ fi
 #  ../scripts/ini2txt.sh  BP_$ialgo
 # done
 
-if [ `echo $LD_LIBRARY_PATH | grep CLA > /dev/null ; echo $?` -ne 0 ]; then
-   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../CLA/
+if [ `echo $LD_LIBRARY_PATH | grep $WORK_PATH/CLA > /dev/null ; echo $?` -ne 0 ]; then
+   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORK_PATH/CLA/
 fi
 
-rm histoOut-BP_*.root 2>/dev/null 
-echo ../CLA/CLA.exe $datafile -inp $datatype -BP $Nalgo -EVT $EVENTS -V ${VERBOSE} -ST $STRT ${DEPS}
-../CLA/CLA.exe $datafile -inp $datatype -BP $Nalgo -EVT $EVENTS -V ${VERBOSE} -ST $STRT ${DEPS}
+rm $PWD/histoOut-BP_*.root 2>/dev/null 
+echo $WORK_PATH/CLA/CLA.exe $datafile -inp $datatype -BP $Nalgo -EVT $EVENTS -V ${VERBOSE} -ST $STRT ${DEPS}
+$WORK_PATH/CLA/CLA.exe $datafile -inp $datatype -BP $Nalgo -EVT $EVENTS -V ${VERBOSE} -ST $STRT ${DEPS}
 if [ $? -eq 0 ]; then
   echo "CutLang finished successfully, now adding histograms"
   rbase=`echo ${INIFILE} | rev | cut -d'/' -f 1 | rev|cut -f1 -d'.'`
-  if [ -f "histoOut-${rbase}.root" ]; then
-    rm -f  histoOut-${rbase}.root
+  if [ -f "$PWD/histoOut-${rbase}.root" ]; then
+    rm -f  $PWD/histoOut-${rbase}.root
   fi
-  hadd histoOut-${rbase}.root histoOut-BP_*.root
-  if [ $? -eq 120 ]; then
+  hadd $PWD/histoOut-${rbase}.root $PWD/histoOut-BP_*.root
+  if [ $? -eq 0 ]; then
    echo "hadd finished successfully, now removing auxiliary files"
-   rm -f histoOut-BP_*.root
-   rm -f _head.ini _algos.ini _inifile
-   rm -f BP_*-card.ini 
+   rm -f $PWD/histoOut-BP_*.root
+   rm -f $PWD/_head.ini $PWD/_algos.ini $PWD/_inifile
+   rm -f $PWD/BP_*-card.ini 
+   rm -f $PWD/out1 
+   rm -f $PWD/algdeps.cmd 
   fi
 fi
 
--- scripts/separate_algos.sh
+++ scripts/separate_algos.sh
@@ -1,72 +1,79 @@
 #!/bin/bash
 
-INIFILE=CLA.ini
+_SCRIPTS_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+_SCRIPT=`realpath -s $0`
+SCRIPT=`realpath $0`            # /home/.../CutLang/scripts/seperate_algos.sh
+SCRIPTS_PATH=`dirname $SCRIPT`    # /home/.../CutLang/scripts
+WORK_PATH=`dirname $SCRIPTS_PATH`  # /home/.../CutLang
+RUNS_PATH=$WORK_PATH/runs  # /home/.../CutLang/runs
+
+INIFILE=$RUNS_PATH/CLA.ini
 
 if [ $# == 1 ]; then
  INIFILE=$1
 fi
 
-cat $INIFILE | grep -v '^ *#' > _inifile
+cat $INIFILE | grep -v '^ *#' > $PWD/_inifile
 #-------remove all lines starting with a #
-INIFILE=_inifile
+INIFILE=$PWD/_inifile
 
 # replace all region keyword with algo
-sed 's/region /algo /g'  $INIFILE > pippo
-mv  pippo _inifile 
+sed 's/region /algo /g'  $INIFILE > $PWD/pippo
+mv  $PWD/pippo $PWD/_inifile 
 
 inihead=`cat ${INIFILE} | grep -n -m 1 algo | cut -f1 -d":"`
 initot=`wc -l ${INIFILE} |  awk '{ print $1 }'` 
 
-head -n $inihead ${INIFILE} | grep -v algo > _head.ini
-tail -n $(( initot - $inihead +1 )) ${INIFILE} > _algos.ini
-rm -rf out1
+head -n $inihead ${INIFILE} | grep -v algo > $PWD/_head.ini
+tail -n $(( initot - $inihead +1 )) ${INIFILE} > $PWD/_algos.ini
+rm -rf $PWD/out1
 
-algo_list=`cat _algos.ini | grep -n algo | cut -f1 -d":"`
+algo_list=`cat $PWD/_algos.ini | grep -n algo | cut -f1 -d":"`
 
 iregion=0
 for an_algo in $algo_list ; do
  iregion=$(( iregion + 1 ))
  algo1p=$(( an_algo + 1  ))
- thisline=`head -n $an_algo _algos.ini| tail -n 1`
- nextline=`head -n $algo1p  _algos.ini| tail -n 1`
+ thisline=`head -n $an_algo $PWD/_algos.ini| tail -n 1`
+ nextline=`head -n $algo1p  $PWD/_algos.ini| tail -n 1`
  depalgo=-1
  mainalgo=-1
 
 ### echo $an_algo  $thisline $nextline
 
  this_region_name=`echo $thisline | cut -f2 -d ' '`
- next_rline=`cat _algos.ini | grep -n -m $(( 1 + iregion )) algo | tail -1| cut -f1 -d":"`
+ next_rline=`cat $PWD/_algos.ini | grep -n -m $(( 1 + iregion )) algo | tail -1| cut -f1 -d":"`
  if [ $next_rline == $an_algo ]; then
-   next_rline=`wc -l _algos.ini |  awk '{ print $1+1 }'`
+   next_rline=`wc -l $PWD/_algos.ini |  awk '{ print $1+1 }'`
  fi
  printf 'A region, ID:%3s named:%20s \t is defined in lines from %d to %d\n' "$((iregion -1 ))" "[$this_region_name]" "$an_algo" "$next_rline"
 
  if [[ $nextline != *"cmd "* ]] && [[ $nextline != *"select "* ]] && [[ $nextline != *"histo "* ]] && [[ $nextline != *"weight "* ]]; then
   echo "   this region depends on:"  $nextline
   cat $nextline > $this_region_name
-  awk -v lstart=$an_algo -v lfinish=$next_rline  'NR > lstart+1 && NR < lfinish' _algos.ini >> $this_region_name 
+  awk -v lstart=$an_algo -v lfinish=$next_rline  'NR > lstart+1 && NR < lfinish' $PWD/_algos.ini >> $this_region_name 
   depalgo=$((iregion -1 ))
  else
-  awk -v lstart=$an_algo -v lfinish=$next_rline  'NR > lstart && NR < lfinish' _algos.ini > $this_region_name 
+  awk -v lstart=$an_algo -v lfinish=$next_rline  'NR > lstart && NR < lfinish' $PWD/_algos.ini > $this_region_name 
   mainalgo=$((iregion -1 ))
  fi
- cat _head.ini > BP_${iregion}-card.ini
- echo $thisline >> BP_${iregion}-card.ini
- cat $this_region_name >> BP_${iregion}-card.ini
+ cat $PWD/_head.ini > $PWD/BP_${iregion}-card.ini
+ echo $thisline >> $PWD/BP_${iregion}-card.ini
+ cat $this_region_name >> $PWD/BP_${iregion}-card.ini
  
 # echo  $mainalgo ":" $depalgo
  if [[ $mainalgo -gt -1 ]] && [[ $depalgo -eq -1 ]]; then
-   echo -n ${mainalgo}":">> out1
+   echo -n ${mainalgo}":">> $PWD/out1
  fi
  if [[ $mainalgo -eq -1 ]] && [[ $depalgo -gt -1 ]]; then
-   echo -n  $depalgo",">> out1
+   echo -n  $depalgo",">> $PWD/out1
  fi
 
 done
-echo >> out1
-depregs=`cat out1 | grep ','| sed 's/.$//'`
+echo >> $PWD/out1
+depregs=`cat $PWD/out1 | grep ','| sed 's/.$//'`
 if [ ${#depregs} -gt 0 ]; then
- echo "-P ${depregs}" >algdeps.cmd
+ echo "-P ${depregs}" >$PWD/algdeps.cmd
 fi
 
 # CLEAN the region names
