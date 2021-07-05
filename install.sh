cf521571d6801c1081f7d4974dd2c13975643ad3
Burak Shen <tobuba2@gmail.com>
2021-07-05 15:03:23 +0300

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 NAME=$2
 if [ $1 = "install" ]; then
     mkdir -p ~/CutLang_repo
-    conda env create -f https://raw.githubusercontent.com/shenburak/CutLang/master/environment.yml --name $NAME
-    conda activate $NAME
-    git clone https://github.com/shenburak/CutLang ~/CutLang_repo
-    cd ~/CutLang_repo/CLA
-    make
-    source ~/CutLang_repo
-    echo "source ~/CutLang_repo" >> ~/.bashrc
-    echo "Now you can use CutLang with CLA command"
+    #git clone https://github.com/shenburak/CutLang ~/CutLang_repo
+    #conda env create -f https://raw.githubusercontent.com/shenburak/CutLang/master/environment.yml --name $NAME
+    #source activate $NAME && cd ~/CutLang_repo/CLA && make
+    source ~/CutLang_repo/setup.sh
+    echo "source ~/CutLang_repo/setup.sh" >> ~/.bashrc
+    echo "~~~~~~~~ Now you can use CutLang with CLA command ~~~~~~~~"
 elif [ $1 = "remove" ]; then
     echo "remove"
 else
