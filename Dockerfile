b749893ab606f5ff253bd72a27e1590580c0742c
Burak Shen <tobuba2@gmail.com>
2021-07-16 02:29:21 +0300

    bug fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM rootproject/root:6.24.00-ubuntu20.04
 
+WORKDIR /CutLang
+COPY . .
+
 RUN apt update
 
 RUN apt install python3-pip -y
@@ -7,12 +10,14 @@ RUN pip install jupyterlab metakernel
 RUN mkdir -p $(jupyter --data-dir)/kernels/cutlang
 
 RUN apt install -y bison flex make
-RUN echo "alias python=python3" >> ~/.bashrc
-RUN echo "source /src/setup.sh" >> ~/.bashrc
 
-RUN echo "cp /src/kernel/kernel.json $(jupyter --data-dir)/kernels/cutlang/kernel.json" >> ~/.bashrc
-RUN echo "cp /src/kernel/magics/cutlangmagic.py $ROOTSYS/lib/JupyROOT/kernel/magics/cutlangmagic.py" >> ~/.bashrc
-RUN echo "cp /src/kernel/cutlangmagic.py $ROOTSYS/lib/JupyROOT/magics/cutlangmagic.py" >> ~/.bashrc
+RUN cd /CutLang/CLA && make clean && make
+RUN echo "source /CutLang/setup.sh" >> ~/.bashrc
+RUN echo "alias python=python3" >> ~/.bashrc
+
+RUN echo "cp /CutLang/kernel/kernel.json $(jupyter --data-dir)/kernels/cutlang/kernel.json" >> ~/.bashrc
+RUN echo "cp /CutLang/kernel/magics/cutlangmagic.py $ROOTSYS/lib/JupyROOT/kernel/magics/cutlangmagic.py" >> ~/.bashrc
+RUN echo "cp /CutLang/kernel/cutlangmagic.py $ROOTSYS/lib/JupyROOT/magics/cutlangmagic.py" >> ~/.bashrc
 
 WORKDIR /src
 
--- kernel/magics/cutlangmagic.py
+++ kernel/magics/cutlangmagic.py
@@ -11,16 +11,19 @@ class CutLangMagics(Magic):
     @option('name', action="store", help='HistoOut file name')
     def cell_cutlang(self, __args): # real
         _args = __args.split(" ")
-        args = {"file": "", "filetype": "", "name": ""}
+        args = {"file": "", "filetype": "", "name": "cutlang-adl-name"}
         for arg in _args:
             args[arg.split("=")[0].replace("-", "")]=arg.split("=")[1]
         if self.code.strip():
             PWD = sp.getoutput('pwd')
-            f = open("/tmp/" + args["name"], "a")
+            adlName = "/tmp/" + args["name"] + ".adl"
+            print(adlName)
+            f = open(adlName, "a")
             f.write(self.code)
             f.close()
-            print(PWD, "pwd", 'CLA ' + PWD + "/" + args["file"] + ' ' + args["filetype"] + ' -i ' + tf.name)
-            os.system('CLA ' + PWD + "/" + args["file"] + ' ' + args["filetype"] + ' -i ' + tf.name)
+            print(PWD, "pwd", 'CLA ' + PWD + "/" + args["file"] + ' ' + args["filetype"] + ' -i ' + adlName)
+            os.system('CLA ' + PWD + "/" + args["file"] + ' ' + args["filetype"] + ' -i ' + adlName)
+            os.remove(adlName)
         self.evaluate = False
 
 def register_magics(kernel):
