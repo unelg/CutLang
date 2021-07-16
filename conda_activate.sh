c4bae85a2e4c4033dd5a3aab294e944e52f54ead
Burak Shen <tobuba2@gmail.com>
2021-07-17 00:28:40 +0300

    shell magic added

--- conda_activate.sh
+++ conda_activate.sh
@@ -1,15 +1,22 @@
 export CUTLANG_PATH=$CONDA_PREFIX/bin/cutlang
 export PATH=$CUTLANG_PATH/runs:$PATH
+export CUTLANG_JUPYTER_DATA_DIR=$(jupyter --data-dir)
 
-mkdir -p $(jupyter --data-dir)/kernels/cutlang
+mkdir -p $CUTLANG_JUPYTER_DATA_DIR/kernels/cutlang
 
-cp $CUTLANG_PATH/kernel/kernel.json $(jupyter --data-dir)/kernels/cutlang/kernel.json
+cp $CUTLANG_PATH/kernel/kernel.json $CUTLANG_JUPYTER_DATA_DIR/kernels/cutlang/kernel.json
 
 cp $CUTLANG_PATH/kernel/magics/cutlangmagic.py $ROOTSYS/lib/JupyROOT/kernel/magics/cutlangmagic.py 2>>/dev/null
 cp $CUTLANG_PATH/kernel/magics/cutlangmagic.py $ROOTSYS/lib/python3.9/site-packages/JupyROOT/kernel/magics/cutlangmagic.py 2>>/dev/null
 
+cp $CUTLANG_PATH/kernel/magics/shellmagic.py $ROOTSYS/lib/JupyROOT/kernel/magics/shellmagic.py 2>>/dev/null
+cp $CUTLANG_PATH/kernel/magics/shellmagic.py $ROOTSYS/lib/python3.9/site-packages/JupyROOT/kernel/magics/shellmagic.py 2>>/dev/null
+
 cp $CUTLANG_PATH/kernel/cutlangmagic.py $ROOTSYS/lib/JupyROOT/magics/cutlangmagic.py 2>>/dev/null
 cp $CUTLANG_PATH/kernel/cutlangmagic.py $ROOTSYS/lib/python3.9/site-packages/JupyROOT/magics/cutlangmagic.py 2>>/dev/null
+
+cp $CUTLANG_PATH/kernel/shellmagic.py $ROOTSYS/lib/JupyROOT/magics/shellmagic.py 2>>/dev/null
+cp $CUTLANG_PATH/kernel/shellmagic.py $ROOTSYS/lib/python3.9/site-packages/JupyROOT/magics/shellmagic.py 2>>/dev/null
 
 echo "For examples see $CUTLANG_PATH/runs/"
 echo "and for LHC analysis implementations, see"
--- conda_deactivate.sh
+++ conda_deactivate.sh
@@ -3,12 +3,19 @@ PATH=:$PATH:
 PATH=${PATH//:$directory_to_remove:/:}
 PATH=${PATH#:}; PATH=${PATH%:}
 
-rm -rf $(jupyter --data-dir)/kernels/cutlang
+#rm -rf $CUTLANG_JUPYTER_DATA_DIR/kernels/cutlang
 
-rm $ROOTSYS/lib/JupyROOT/kernel/magics/cutlangmagic.py 2>>/dev/null
-rm $ROOTSYS/lib/python3.9/site-packages/JupyROOT/kernel/magics/cutlangmagic.py 2>>/dev/null
+#rm $ROOTSYS/lib/JupyROOT/kernel/magics/cutlangmagic.py 2>>/dev/null
+#rm $ROOTSYS/lib/python3.9/site-packages/JupyROOT/kernel/magics/cutlangmagic.py 2>>/dev/null
 
-rm $ROOTSYS/lib/JupyROOT/magics/cutlangmagic.py 2>>/dev/null
-rm $ROOTSYS/lib/python3.9/site-packages/JupyROOT/magics/cutlangmagic.py 2>>/dev/null
+#rm $ROOTSYS/lib/JupyROOT/magics/cutlangmagic.py 2>>/dev/null
+#rm $ROOTSYS/lib/python3.9/site-packages/JupyROOT/magics/cutlangmagic.py 2>>/dev/null
+
+#rm $ROOTSYS/lib/JupyROOT/kernel/magics/shellmagic.py 2>>/dev/null
+#rm $ROOTSYS/lib/python3.9/site-packages/JupyROOT/kernel/magics/shellmagic.py 2>>/dev/null
+
+#rm $ROOTSYS/lib/JupyROOT/magics/shellmagic.py 2>>/dev/null
+#rm $ROOTSYS/lib/python3.9/site-packages/JupyROOT/magics/shellmagic.py 2>>/dev/null
 
 unset CUTLANG_PATH
+unset CUTLANG_JUPYTER_DATA_DIR
