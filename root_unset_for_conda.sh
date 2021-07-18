c3baccdf0381cac114e417b9453105c2e350cd6c
Burak Shen <tobuba2@gmail.com>
2021-07-18 03:42:56 +0300

    conda root fix

--- root_unset_for_conda.sh
+++ root_unset_for_conda.sh
@@ -83,100 +83,14 @@ clean_environment()
    fi
 }
 
-set_environment()
-{
-   if [ -z "${PATH}" ]; then
-      PATH=$ROOTSYS/bin; export PATH
-   else
-      PATH=$ROOTSYS/bin:$PATH; export PATH
-   fi
-
-   if [ -z "${LD_LIBRARY_PATH}" ]; then
-      LD_LIBRARY_PATH=$ROOTSYS/lib
-      export LD_LIBRARY_PATH       # Linux, ELF HP-UX
-   else
-      LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
-      export LD_LIBRARY_PATH
-   fi
-
-   #if [ -z "${DYLD_LIBRARY_PATH}" ]; then
-   #   DYLD_LIBRARY_PATH=$ROOTSYS/lib
-   #   export DYLD_LIBRARY_PATH       # Linux, ELF HP-UX
-   #else
-   #   DYLD_LIBRARY_PATH=$ROOTSYS/lib:$DYLD_LIBRARY_PATH
-   #   export DYLD_LIBRARY_PATH
-   #fi
-
-   #if [ -z "${SHLIB_PATH}" ]; then
-   #   SHLIB_PATH=$ROOTSYS/lib
-   #   export SHLIB_PATH       # Linux, ELF HP-UX
-   #else
-   #   SHLIB_PATH=$ROOTSYS/lib:$SHLIB_PATH
-   #   export SHLIB_PATH
-   #fi
-
-   #if [ -z "${LIBPATH}" ]; then
-   #   LIBPATH=$ROOTSYS/lib
-   #   export LIBPATH       # Linux, ELF HP-UX
-   #else
-   #   LIBPATH=$ROOTSYS/lib:$LIBPATH
-   #   export LIBPATH
-   #fi
-
-   #if [ -z "${PYTHONPATH}" ]; then
-      PYTHONPATH=$ROOTSYS/lib
-      export PYTHONPATH       # Linux, ELF HP-UX
-   #else
-   #   PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
-   #   export PYTHONPATH
-   #fi
-
-   #if [ -z "${MANPATH}" ]; then
-   #   MANPATH=$ROOTSYS/man:${default_manpath}; export MANPATH
-   #else
-   #   MANPATH=$ROOTSYS/man:$MANPATH; export MANPATH
-   #fi
-
-   #if [ -z "${CMAKE_PREFIX_PATH}" ]; then
-   #   CMAKE_PREFIX_PATH=$ROOTSYS; export CMAKE_PREFIX_PATH       # Linux, ELF HP-UX
-   #else
-   #   CMAKE_PREFIX_PATH=$ROOTSYS:$CMAKE_PREFIX_PATH; export CMAKE_PREFIX_PATH
-   #fi
-
-   #if [ -z "${JUPYTER_PATH}" ]; then
-      JUPYTER_PATH=$ROOTSYS/etc/notebook; export JUPYTER_PATH       # Linux, ELF HP-UX
-   #else
-   #   JUPYTER_PATH=$ROOTSYS/etc/notebook:$JUPYTER_PATH; export JUPYTER_PATH
-   #fi
-
-   #if [ -z "${JUPYTER_CONFIG_DIR}" ]; then
-      JUPYTER_CONFIG_DIR=$ROOTSYS/etc/notebook; export JUPYTER_CONFIG_DIR # Linux, ELF HP-UX
-   #else
-   #   JUPYTER_CONFIG_DIR=$ROOTSYS/etc/notebook:$JUPYTER_CONFIG_DIR; export JUPYTER_CONFIG_DIR
-   #fi
-}
-
-
-### main ###
-
-
-if [ -n "${ROOTSYS}" ] ; then
-   old_rootsys=${ROOTSYS}
+if [ -n "${_ROOTSYS}" ] ; then
+   old_rootsys=${_ROOTSYS}
 fi
 
-
-#SOURCE=${BASH_ARGV[0]}:${(%):-%N}
-
-export ROOTSYS=$CONDA_PREFIX
-
 clean_environment
-set_environment
-
-# Prevent Cppyy from checking the PCH (and avoid warning)
-export CLING_STANDARD_PCH=none
 
 if [ "x`root-config --arch | grep -v win32gcc | grep -i win32`" != "x" ]; then
-   ROOTSYS="`cygpath -w $ROOTSYS`"
+   _ROOTSYS="`cygpath -w $_ROOTSYS`"
 fi
 
 unset old_rootsys
