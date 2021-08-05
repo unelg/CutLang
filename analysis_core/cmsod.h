0154111397236ec1a7d9af6ff772e0492d99cbcf
unelg <gokhan.unel@gmail.com>
2021-08-05 10:37:44 +0200

    cmsod r2 added

--- analysis_core/cmsod.h
+++ analysis_core/cmsod.h
@@ -131,13 +131,10 @@ public :
 
    cmsod(char *file_name, TChain *tree=0);
    virtual ~cmsod();
-   virtual Int_t    Cut(Long64_t entry);
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
    virtual void     Loop(analy_struct aselect, char *sampletype);
-   virtual Bool_t   Notify();
-   virtual void     Show(Long64_t entry = -1);
 };
 
 #endif
@@ -180,7 +177,6 @@ Long64_t cmsod::LoadTree(Long64_t entry)
    if (centry < 0) return centry;
    if (fChain->GetTreeNumber() != fCurrent) {
       fCurrent = fChain->GetTreeNumber();
-      Notify();
    }
    return centry;
 }
@@ -252,32 +248,6 @@ void cmsod::Init(TTree *tree)
    fChain->SetBranchAddress("NPrimaryVertices", &NPrimaryVertices, &b_NPrimaryVertices);
    fChain->SetBranchAddress("triggerIsoMu24", &triggerIsoMu24, &b_triggerIsoMu24);
    fChain->SetBranchAddress("EventWeight", &EventWeight, &b_EventWeight);
-   Notify();
 }
 
-Bool_t cmsod::Notify()
-{
-   // The Notify() function is called when a new file is opened. This
-   // can be either for a new TTree in a TChain or when when a new TTree
-   // is started when using PROOF. It is normally not necessary to make changes
-   // to the generated code, but the routine can be extended by the
-   // user if needed. The return value is currently not used.
-
-   return kTRUE;
-}
-
-void cmsod::Show(Long64_t entry)
-{
-// Print contents of entry.
-// If entry is not specified, print current entry
-   if (!fChain) return;
-   fChain->Show(entry);
-}
-Int_t cmsod::Cut(Long64_t entry)
-{
-// This function may be called from Loop.
-// returns  1 if entry is accepted.
-// returns -1 otherwise.
-   return 1;
-}
 #endif // #ifdef cmsod_cxx
