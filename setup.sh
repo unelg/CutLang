9b681d81ffbb79acd367d9295aab02126accdda6
Burak Shen <tobuba2@gmail.com>
2021-08-05 21:12:51 +0300

    fix minor bug

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export CUTLANG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )"
 export PATH=$CUTLANG_PATH/runs:$PATH
 
-./scripts/setup_kernel.sh
+$CUTLANG_PATH/scripts/setup_kernel.sh
