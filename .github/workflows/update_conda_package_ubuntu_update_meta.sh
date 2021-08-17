f9fc88b9c823020cd0233c55380b00d8048c02d3
Burak Shen <tobuba2@gmail.com>
2021-08-17 16:42:44 +0300

    export meta

--- .github/workflows/update_conda_package_ubuntu_update_meta.sh
+++ .github/workflows/update_conda_package_ubuntu_update_meta.sh
@@ -2,7 +2,7 @@ export version_cutlang=$(echo cat ../../VERSION)
 
 sed -i "/version = /c{% set version = '$version_cutlang' %}" meta.yaml
 sed -i "/cout << \"CLA v/c cout << \"CLA v$version_cutlang\\\t\";" ../../CLA/CLA.Q
-meta=$(cat ../../scripts/conda/meta.yaml)
+export meta=$(cat ../../scripts/conda/meta.yaml)
 
 sed -i "/cutlang ==/c  - cutlang ==$version_cutlang" ../../binder/environment.yml
 
