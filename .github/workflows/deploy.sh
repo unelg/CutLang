01b77221092432c67228f98f43bf43f3100e82bd
Burak Shen <tobuba2@gmail.com>
2021-09-01 15:53:56 +0300

    fixed deploy.sh path

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $HOME/work/CutLang/CutLang/runs
+cd ../../runs
 wget http://www.hepforge.org/archive/cutlang/cms_opendata_ttbar.root
 wget http://www.hepforge.org/archive/cutlang/atla_opendata_had_ttbar.root
 wget -O delphes_events_ttbar.root https://docs.google.com/uc?export=download\&id=1P8Pv2hmV4QcMfNWmQTsuAkqIYcEzsuxt
