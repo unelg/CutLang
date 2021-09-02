5e6627c409bb25e54529c419c052cadd221b5dbe
Burak Shen <tobuba2@gmail.com>
2021-09-02 16:58:12 +0300

    fixed

--- binder/jupyter_notebook_config.py
+++ binder/jupyter_notebook_config.py
@@ -12,13 +12,13 @@ if 'ROOTSYS' in os.environ:
 
     #_rootnbpath = os.environ['HOME'] + "/.rootnb"
     
-    os.environ['PATH']            = '%s:%s/bin' % (_rootbin,_rootbin) + ':' + os.getenv('PATH', '')
-    os.environ['LD_LIBRARY_PATH'] = '%s' % _rootlib + ':' + os.getenv('LD_LIBRARY_PATH', '')
-    os.environ['DYLD_LIBRARY_PATH'] = '%s' % _rootlib + ':' + os.getenv('DYLD_LIBRARY_PATH', '')
-    os.environ['SHLIB_PATH'] = '%s' % _rootlib + ':' + os.getenv('SHLIB_PATH', '')
-    os.environ['LIB_PATH'] = '%s' % _rootlib + ':' + os.getenv('LIB_PATH', '')
-    os.environ['PYTHONPATH']      = '%s' % _rootlib + ':' + os.getenv('PYTHONPATH', '')
-    os.environ['CMAKE_PREFIX_PATH']      = '%s' % _rootdata + ':' + os.getenv('CMAKE_PREFIX_PATH', '')
+    #os.environ['PATH']            = '%s:%s/bin' % (_rootbin,_rootbin) + ':' + os.getenv('PATH', '')
+    #os.environ['LD_LIBRARY_PATH'] = '%s' % _rootlib + ':' + os.getenv('LD_LIBRARY_PATH', '')
+    #os.environ['DYLD_LIBRARY_PATH'] = '%s' % _rootlib + ':' + os.getenv('DYLD_LIBRARY_PATH', '')
+    #os.environ['SHLIB_PATH'] = '%s' % _rootlib + ':' + os.getenv('SHLIB_PATH', '')
+    #os.environ['LIB_PATH'] = '%s' % _rootlib + ':' + os.getenv('LIB_PATH', '')
+    #os.environ['PYTHONPATH']      = '%s' % _rootlib + ':' + os.getenv('PYTHONPATH', '')
+    #os.environ['CMAKE_PREFIX_PATH']      = '%s' % _rootdata + ':' + os.getenv('CMAKE_PREFIX_PATH', '')
     #os.environ['JUPYTER_PATH']      = '%s' % _rootnbpath + ':' + os.getenv('JUPYTER_PATH', '')
     #os.environ['JUPYTER_CONFIG_DIR']      = '%s' % _rootnbpath + ':' + os.getenv('JUPYTER_CONFIG_DIR', '')
     c.NotebookApp.extra_static_paths.append(os.path.join(os.environ['ROOTSYS'], 'js/'))
