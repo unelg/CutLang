c713c3ec520d8f52ae4347f0cc7bb7df077c4cd1
Burak Shen <tobuba2@gmail.com>
2021-09-02 14:14:23 +0300

    added just_setup

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,7 +6,7 @@ wget http://www.hepforge.org/archive/cutlang/cms_opendata_ttbar.root
 wget http://www.hepforge.org/archive/cutlang/atla_opendata_had_ttbar.root
 wget -O delphes_events_ttbar.root https://docs.google.com/uc?export=download\&id=1P8Pv2hmV4QcMfNWmQTsuAkqIYcEzsuxt
 
-source ../setup.sh
+source ../scripts/just_setup.sh
 
 for i in $(ls |grep .adl); do
         echo "Testing $i:"
