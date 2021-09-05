ca9133110f9ecb53ea8f40efd23cf116db01e8c0
Burak Shen <tobuba2@gmail.com>
2021-09-05 11:49:38 +0300

    python tests reopened

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -20,14 +20,14 @@ for i in $(ls |grep .adl); do
         echo "With DELPHES"
         echo "CLA ./delphes_events_ttbar.root DELPHES -i $i -e 1000 -v 500"
         CLA ./delphes_events_ttbar.root DELPHES -i $i -e 1000 -v 500
-        #echo "Using Python Scripts"
-        #echo "With CMSOD"
-        #echo "python3 CLA.py ./cms_opendata_ttbar.root CMSOD -i $i -e 1000 -v 500"
-        #python3 CLA.py ./cms_opendata_ttbar.root CMSOD -i $i -e 1000 -v 500
-        #echo "With ATLASOD"
-        #echo "python3 CLA.py ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 1000 -v 500"
-        #python3 CLA.py ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 1000 -v 500
-        #echo "With DELPHES"
-        #echo "python3 CLA.py ./delphes_events_ttbar.root DELPHES -i $i -e 1000 -v 500"
-        #python3 CLA.py ./delphes_events_ttbar.root DELPHES -i $i -e 1000 -v 500
+        echo "Using Python Scripts"
+        echo "With CMSOD"
+        echo "python3 CLA.py ./cms_opendata_ttbar.root CMSOD -i $i -e 1000 -v 500"
+        python3 CLA.py ./cms_opendata_ttbar.root CMSOD -i $i -e 1000 -v 500
+        echo "With ATLASOD"
+        echo "python3 CLA.py ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 1000 -v 500"
+        python3 CLA.py ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 1000 -v 500
+        echo "With DELPHES"
+        echo "python3 CLA.py ./delphes_events_ttbar.root DELPHES -i $i -e 1000 -v 500"
+        python3 CLA.py ./delphes_events_ttbar.root DELPHES -i $i -e 1000 -v 500
 done
