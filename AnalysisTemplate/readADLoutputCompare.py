53167674ae4a291262e6ad2dbb996e50805e82d3
Ronja Öhrnberg <ronjaohrnberg@gmail.com>
2022-07-07 16:59:06 +0300

    Edited rebin and indexing, recognizes and ignores comments

--- AnalysisTemplate/readADLoutputCompare.py
+++ AnalysisTemplate/readADLoutputCompare.py
@@ -28,7 +28,7 @@ def ask_outfile():
     return outfname
         
 
-fileinpath = input('Give an ADL file: ') 
+fileinpath = '../../cern2022/VLL-2LOS.adl' #input('Give an ADL file: ') 
 while not exists(fileinpath):
     print('File not found!')
     fileinpath = input('Give an ADL file: ') 
@@ -36,29 +36,34 @@ infile = open(fileinpath,'r')
 lines=infile.readlines()
 infile.close()
 
-outfname = ask_outfile()
+outfname = 'VLL_compare.dat' #ask_outfile()
 print('Writing to file',outfname)
 outf = open(outfname,'w')
 
 histos = []
 for line in lines:
     if 'histo' in line:
-        line = line.replace(' ','')
-        line = line.replace('\n','')
-        line = line.replace('histo','')
-        line = line.split(',')
-        histos.append(line)
+        hashornot = line.split('histo')
+        if '#' not in hashornot[0]:
+            line = line.replace(' ','')
+            line = line.replace('\n','')
+            line = line.replace('histo','')
+            line = line.split(',')
+            histos.append(line)
 
-numbers = np.arange(8,8+len(histos),1)
+numbers = np.arange(1,1+len(histos),1)
 for n in numbers:
     psetout = ' PSET'+str(n)+' = 1\n'
-    muonout = ' MUON'+str(n)+' = '+histos[n-8][0]+'\n'
-    eleout = ' ELE'+str(n)+' = '+histos[n-8][0]+'\n'
-    rebinout = 'rebin'+str(n)+' = '+histos[n-8][2]+'\n'
-    minmaxout = 'minmax'+str(n)+' = '+histos[n-8][3]+','+histos[n-8][4]+'\n'
+    muonout = ' MUON'+str(n)+' = '+histos[n-1][0]+'\n'
+    eleout = ' ELE'+str(n)+' = '+histos[n-1][0]+'\n'
+    rebinout = 'rebin'+str(n)+' = '+str(1)+'\n'
+    minmaxout = 'minmax'+str(n)+' = '+histos[n-1][3]+','+histos[n-1][4]+'\n'
     lineout = psetout+muonout+eleout+rebinout+minmaxout+'\n'
     outf.write(lineout)
-
 outf.close()
 
-
+msg = 'Printed histograms ['
+for n in numbers[:-1]:
+    msg += str(n)+', '
+msg += str(numbers[-1])+'] to a file.'
+print(msg)
