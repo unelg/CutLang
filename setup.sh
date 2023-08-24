e86e5b59a76982d3ec47bfc1ba7e5332f7f4279a
Gokhan Unel <gokhan.unel@cern.ch>
2023-08-24 10:09:46 +0200

    ipython check

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 export CUTLANG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )"
 export PATH=$CUTLANG_PATH/runs:$CUTLANG_PATH/scripts:$PATH
 
-isProd="${1:-1}"
-source $CUTLANG_PATH/scripts/setup_kernel.sh $ROOTSYS $isProd
+
+if python -c "import IPython" &> /dev/null; then
+    echo "IPython is installed."
+    isProd="${1:-1}"
+    source $CUTLANG_PATH/scripts/setup_kernel.sh $ROOTSYS $isProd
+    echo done.
+else
+    echo "Done. But, IPython is not installed. You might want to install it if you want to use jupiter notebooks."
+fi
+
